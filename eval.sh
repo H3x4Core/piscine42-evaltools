f6812da3e68b0247e2da190861c704d0e2c1b743
Hexa_Core <hexa_core@bluewin.ch>
2022-06-11 13:06:59 +0200

    update

--- eval.sh
+++ eval.sh
@@ -1,16 +1,18 @@
 #!/bin/zsh
 
 PROJECT=C42
-FOLDER=~/code/piscine42
+FOLDER=~/code
 TMPCLONE=~/code/tmp-evaltools
 
 #git clone address $FOLDER/$PROJECT
 git clone git@github.com:H3x4Core/piscine42-evaltools.git $TMPCLONE
 
 cd $FOLDER/$PROJECT
-find * -type f | grep -v 'ft_'
 
+find * -type f | grep -v 'ft_'
 norminette -R CheckForbiddenSourceHeader
 
-cp -r $TMPCLONE/$PROJECT/* $FOLDER/$PROJECT/
-rm -rf $TMPCLONE
+find $TMPCLONE/$PROJECT/ -name 'main.c' -exec cp -r "{}" $FOLDER/$PROJECT  \;
+
+#cp -r $TMPCLONE/$PROJECT/* $FOLDER/$PROJECT/
+#rm -rf $TMPCLONE
