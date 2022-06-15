48c7258fd42a5702cd8f3aa8cce34d47613eee09
Hexa_Core <hexa_core@bluewin.ch>
2022-06-15 14:39:32 +0200

    c04 ex02 remove overflow in main

--- C04/ex02/main.c
+++ C04/ex02/main.c
@@ -4,7 +4,7 @@
 int	main(void)
 {	
 	int nbr[5] = {0, 1, -1, -2147483648, 2147483647};
-	for (long int i = 0; i < 6; i++)
+	for (long int i = 0; i < 5; i++)
 	{
 		write(1,"ft_putnbr: ", 11);
 		ft_putnbr(nbr[i]);
